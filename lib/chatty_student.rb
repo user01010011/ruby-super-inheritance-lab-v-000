217702f6724bbeca65b748fa97d8fc683549e555
Shasha Yan <shashayan@gmail.com>
2021-05-18 17:42:35 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,6 +7,14 @@ class ChattyStudent < Student
 
   def raise_hand
     super
-    10 times.do
-    end
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+    super
+  end
 end
